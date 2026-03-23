2bf315658b2347f1a58727b4250e27d04bdc5b4d
Malak Ahmed <malak.hamdymohamed@student.giu-uni.de>
2026-03-23 15:49:21 +0200

    completed the dataLoader class

--- GIU-game/src/game/engine/dataloader/DataLoader.java
+++ GIU-game/src/game/engine/dataloader/DataLoader.java
@@ -15,80 +15,112 @@ public class DataLoader {
 	
 	public static ArrayList<Card> readCards() throws IOException{
 		ArrayList<Card> cards = new ArrayList<>();
-		BufferedReader br = new BufferedReader(new FileReader(CARDS_FILE_NAME));
-		String line;
-		while((line = br.readLine()) != null){
-			String[] data = line.split(",");
-			
-			String cardType = data[0];
-			String name = data[1];
-			String description = data[2];
-			int rarity = Integer.parseInt(data[3]);
-		try{
-			switch(cardType){
-				case "SWAPPER" :
-					cards.add(new SwapperCard(name, description, rarity));
-					break;
-				case "STARTOVER" :
-					boolean lucky = Boolean.parseBoolean(data[4]);
-					cards.add(new StartOverCard(name, description, rarity, lucky));
-					break;
-				case "ENERGYSTEAL" :
-					int energy = Integer.parseInt(data[4]);
-					cards.add(new EnergyStealCard(name, description, rarity, energy));
-					break;
-				case "SHIELD" :
-					cards.add(new ShieldCard(name, description, rarity));
-					break;
-				case "CONFUSION" :
-					int duration = Integer.parseInt(data[4]);
-					cards.add(new ConfusionCard(name, description, rarity, duration));
-					break;
-					default: throw new InvalidCSVFormat(line);
+		try(BufferedReader br = new BufferedReader(new FileReader(CARDS_FILE_NAME))){
+			String line;
+			while((line = br.readLine()) != null){
+				String[] data = line.split(",");
+				try{
+					String cardType = data[0].trim();
+					String name = data[1].trim();
+					String description = data[2].trim();
+					int rarity = Integer.parseInt(data[3].trim());
+					switch(cardType){
+						case "SWAPPER" :
+							cards.add(new SwapperCard(name, description, rarity));
+							break;
+						case "STARTOVER" :
+							boolean lucky = Boolean.parseBoolean(data[4]);
+							cards.add(new StartOverCard(name, description, rarity, lucky));
+							break;
+						case "ENERGYSTEAL" :
+							int energy = Integer.parseInt(data[4]);
+							cards.add(new EnergyStealCard(name, description, rarity, energy));
+							break;
+						case "SHIELD" :
+							cards.add(new ShieldCard(name, description, rarity));
+							break;
+						case "CONFUSION" :
+							int duration = Integer.parseInt(data[4]);
+							cards.add(new ConfusionCard(name, description, rarity, duration));
+							break;
+							default: throw new InvalidCSVFormat(line);
+					}
+				}
+				catch(ArrayIndexOutOfBoundsException | IllegalArgumentException e){
+					throw new InvalidCSVFormat(line);
+				}
 			}
-		}
-		catch(ArrayIndexOutOfBoundsException | IllegalArgumentException e){
-		throw new InvalidCSVFormat(line);}
-		}
-		br.close();
+		} catch (IOException e){
+        	throw new RuntimeException("Could not read file: " + CARDS_FILE_NAME, e);
+        }
 		return cards;
 	}
 
 
  public static ArrayList<Cell> readCells() throws IOException {
         ArrayList<Cell> cells = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader(CELLS_FILE_NAME));
-        String line;
-        while ((line = br.readLine())!= null) {
-            if (line.trim().isEmpty()) continue;
-            String[] fields = line.split(",");
-			 String name = fields[0].trim();
-            try {
-                if (fields.length == 3) {
-                        Role role = Role.valueOf(fields[1].trim().toUpperCase());
-                        int energy = Integer.parseInt(fields[2].trim());
-                        cells.add(new DoorCell(name, role, energy));
-                } else if (fields.length == 2) {
-                    int effect = Integer.parseInt(fields[1].trim());
-                    if (effect > 0) {
-                        cells.add(new ConveyorBelt(name, effect));
+        try(BufferedReader br = new BufferedReader(new FileReader(CELLS_FILE_NAME))){
+        	String line;
+            while ((line = br.readLine())!= null) {
+                if (line.trim().isEmpty()) 
+                	continue;
+                String[] data = line.split(",");
+    			String name = data[0].trim();
+                try {
+                    if (data.length == 3) {
+                            Role role = Role.valueOf(data[1].trim().toUpperCase());
+                            int energy = Integer.parseInt(data[2].trim());
+                            cells.add(new DoorCell(name, role, energy));
+                    } else if (data.length == 2) {
+                        int effect = Integer.parseInt(data[1].trim());
+                        if (effect > 0) {
+                            cells.add(new ConveyorBelt(name, effect));
+                        } else {
+                            cells.add(new ContaminationSock(name, effect));
+                        }
                     } else {
-                        cells.add(new ContaminationSock(name, effect));
+                        throw new InvalidCSVFormat(line);
                     }
-                } else {
-                    throw new InvalidCSVFormat(line);
+                }  catch (Exception e){
+                	throw new InvalidCSVFormat(line);
                 }
-            } catch (NumberFormatException e) {
-                throw new InvalidCSVFormat(line);
             }
-			br.close();
+        } catch (IOException e){
+        	throw new RuntimeException("Could not read file: " + CELLS_FILE_NAME, e);
         }
-
-        
         return cells;
-    }
-
+ }
+ 
+ public static ArrayList<Monster> readMonsters() throws IOException{
+	ArrayList<Monster> monsters = new ArrayList<>();
+	try(BufferedReader br = new BufferedReader(new FileReader(MONSTERS_FILE_NAME))){
+		String line;
+		while((line = br.readLine()) != null){
+			if(line.trim().isEmpty())
+				continue;
+			String[] data = line.split(",");
+			try{
+				String monsterType = data[0];
+				String name = data[1].trim();
+				String description = data[2].trim();
+				Role role = Role.valueOf(data[3].trim().toUpperCase());
+				int energy = Integer.parseInt(data[4].trim());
+				
+				switch(monsterType){
+				case "DYNAMO" : monsters.add(new Dynamo(name, description, role, energy)); break;
+				case "SCHEMER" : monsters.add(new Schemer(name, description, role, energy)); break;
+				case "MULTITASKER" : monsters.add(new MultiTasker(name, description, role, energy)); break;
+				case "DASHER" : monsters.add(new Dasher(name, description, role, energy)); break;
+				}
+			} catch (Exception e){
+            	throw new InvalidCSVFormat(line);
+            }
+		}
+	} catch (Exception e){
+		throw new RuntimeException("Could not read file: " + MONSTERS_FILE_NAME, e);
+	}
+	return monsters;
 }
 
-
+}
 
